9615d4c4b5c54a9f2ec84a9a4e30f4301c7fcd26
Zachary Goodman <zacharyrgoodman@gmail.com>
2021-04-23 00:56:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (whisper(string)) {return "I can't hear you!"}
-  else if (shout(string)) {return "YES INDEED!"}
+  if (string === whisper(string)) {return "I can't hear you!"}
+  else if (string === shout(string)) {return "YES INDEED!"}
+  else if (string === "I love you, Grandma.") {return "I love you , too."}
 }
